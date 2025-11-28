daee9e2c1ef5789a05e124ac2a1acd739c2a9e0d
Sayem Ahmed <haphazardsayem@gmail.com>
2025-11-28 18:50:30 +0600

    ProductVault : Base setup

---
+++ src/main/java/com/sayem/ProductVault/ProductVaultApplication.java
@@ -0,0 +1,13 @@
+package com.sayem.ProductVault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sayem/ProductVault/ProductVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayem.ProductVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
